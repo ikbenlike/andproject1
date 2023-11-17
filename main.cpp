bcc8d819da67b0e9bb21a9140ec94687c81ff2fb
ikbenlike <thewatcheriswatchinu@gmail.com>
2023-11-17 23:05:23 +0100

    implement tree

--- main.cpp
+++ main.cpp
@@ -3,21 +3,12 @@
 #include <list>
 #include <algorithm>
 
-enum class CanFit {
-	No,
-	Yes,
-	YesReplace
-};
-
 struct Box {
 	friend std::ostream &operator<<(std::ostream&, Box const&);
 private:
 	double m_x = 0;
 	double m_y = 0;
 	double m_z = 0;
-	bool m_full = false;
-	size_t m_n_inside = 0;
-	Box *m_inner = nullptr;
 public:
 	Box(double x = 0, double y = 0, double z = 0)
 	: m_x{x}, m_y{y}, m_z{z} { }
@@ -33,50 +24,16 @@ public:
 		m_z = tmp;
 	}
 
-	CanFit canFit(Box b) {
-		if(!(m_full && m_n_inside <= b.m_n_inside + 1)) {
-			int i = 0;
-			do {
-				if(b.m_x < m_x && b.m_y < m_y && b.m_z < m_z) {
-					/*std::cout << b << std::endl;
-					std::cout << "fits in: " << std::endl;
-					std::cout << *this << std::endl;*/
-					if(m_full)
-						return CanFit::YesReplace;
-					return CanFit::Yes;
-				}
-			} while(i++ < 2);
-		}
-		return CanFit::No;
-	}
-
-	Box insert(Box b) {
-		Box r = Box();
-		if(m_full) {
-			r = *m_inner;
-		}
-		else {
-			m_inner = new Box();
-		}
-	    m_n_inside = b.m_n_inside + 1;
-		*m_inner = b;
-		m_full = true;
-		return r;
-	}
-	
-	/*bool insert(Box b) {
-		if(!(m_full && n_inside > b.n_inside + 1)) {
-			int i = 0;
-			do {
-				if(b.m_x < m_x && b.m_y < m_y && b.m_z < m_z) {
-					m_full = true;
-					return true;
-				}
-				b.rotate();
-			} while(i++ < 2);
-		}
+	bool canFit(Box b) {
+		size_t i = 0;
+	    do {
+			if(b.m_x < m_x && b.m_y < m_y && b.m_z < m_z) {
+				return true;
+			}
+			b.rotate();
+		} while(i++ < 2);
 		return false;
-		}*/
+	}
 	
 	static Box readBox(std::istream &s) {
 		double x = 0;
@@ -89,13 +46,81 @@ public:
 	}
 };
 
+struct Tree {
+	friend std::ostream &operator<<(std::ostream&, Tree const&);
+private:
+	std::vector<Box> m_data = {};
+	std::vector<Tree> m_children = {};
+
+	void print(std::ostream &s, size_t indent) const {
+		for(size_t i = 0; i < indent; i++) {
+			s << "  ";
+		}
+		s << "| data: " << m_data.size();
+		s << ", children: " << m_children.size();
+		s << std::endl;
+		for(Tree t : m_children) {
+			t.print(s, indent + 1);
+		}
+	}
+	
+	void reduceFits() {
+		/*for(Box b : m_data) {
+			std::cout << b << std::endl;
+			}*/
+		for(auto i = m_data.begin(); i < m_data.end(); i++) {
+			std::vector<Box> new_data = {};
+			for(auto j = i + 1; j < m_data.end(); j++) {
+				if(i->canFit(*j)) {
+					new_data.push_back(*j);
+				}
+			}
+			if(new_data.size() != 0) {
+				m_children.push_back(Tree(new_data));
+				/*std::cout << "Inserting new child node" << std::endl;
+				std::cout << "Parent size: " << m_data.size() << std::endl;
+				std::cout << "Generation size: " << new_data.size() << std::endl;*/
+			}
+		}
+		//std::cout << "final child count: " << m_children.size() << std::endl;
+	}
+public:
+	Tree(std::vector<Box> data = {})
+	: m_data{data} { }
+
+	void buildTree() {
+		reduceFits();
+		std::cout << "Data count: " << m_data.size() << std::endl;
+		std::cout << "Child count: " << m_children.size() << std::endl;
+		for(Tree t : m_children) {
+			t.buildTree();
+		}
+	}
+
+	size_t findLongestBranch() {
+		std::cout << "m_data.size(): " << m_data.size() << std::endl;
+		std::cout << "m_children.size(): " << m_children.size() << std::endl;
+		if(m_children.size() == 0) {
+			return 1;
+		}
+		size_t longest = 0;
+		for(Tree t : m_children) {
+			size_t length = t.findLongestBranch();
+			if(length > longest) {
+				longest = length;
+			}
+		}
+		return longest;
+	}
+
+
+};
+
 std::ostream &operator<<(std::ostream &s, Box const &b) {
 	s << "Box { x=" << b.m_x;
 	s << ", y=" << b.m_y;
 	s << ", z=" << b.m_z;
 	s << " } volume: " << b.volume();
-	s << " full: " << b.m_full;
-	s << " contains: " << b.m_n_inside;
 	return s;
 }
 
@@ -103,52 +128,24 @@ bool operator<(Box const &b1, Box const &b2) {
 	return b1.volume() < b2.volume();
 }
 
+std::ostream &operator<<(std::ostream &s, Tree const &t) {
+	t.print(s, 0);
+	return s;
+}
+
 int main() {
 	size_t nbox = 0;
-	std::vector<Box> sorted_boxes = {};
-	std::list<Box> boxes = {};
+	std::vector<Box> boxes = {};
 	std::cin >> nbox;
-	//std::cout << "amount of boxes: " << nbox << std::endl;
-	sorted_boxes.resize(nbox);
 	boxes.resize(nbox);
 	for(size_t i = 0; i < nbox; i++) {
-		sorted_boxes[i] = Box::readBox(std::cin);
+	    boxes[i] = Box::readBox(std::cin);
 	}
 
-	std::sort(sorted_boxes.begin(), sorted_boxes.end());
-	std::copy(sorted_boxes.begin(), sorted_boxes.end(), boxes.begin());
-	/*for(auto i = boxes.begin(); i != boxes.end();) {
-		bool inc = true;
-		for(auto j = i; j != boxes.end(); j++) {
-			if(j->insert(*i)) {
-				inc = false;
-				i = boxes.erase(i);
-				break;
-			}
-		}
-		if(inc)
-			i++;
-			}*/
-	for(auto i = boxes.begin(); i != boxes.end();) {
-		bool inc = true;
-		for(auto j = i; j != boxes.end(); j++) {
-			CanFit fits = j->canFit(*i);
-			if(fits != CanFit::No) {
-				inc = false;
-				Box tmp = j->insert(*i);
-				if(fits == CanFit::YesReplace) {
-					*i = tmp;
-				}
-				else {
-					i = boxes.erase(i);
-				}
-			}
-		}
-		if(inc)
-			i++;
-	}
-	/*for(Box b : boxes) {
-		std::cout << b << std::endl;
-		}*/
-	std::cout << boxes.size() << std::endl;
+	std::sort(boxes.rbegin(), boxes.rend());
+	Tree tree = Tree(boxes);
+	tree.buildTree();
+	std::cout << tree << std::endl;
+	std::cout << tree.findLongestBranch() << std::endl;
+	//std::cout << boxes.size() << std::endl;
 }
